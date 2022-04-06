57abfecb6b11b3da7800ba5c0f0ce6597a26e318
Mona Lisa <carrebola@fpllefia.com>
2022-04-06 13:32:00 +0200

    hora ok

--- index.js
+++ index.js
@@ -1,3 +1,20 @@
+//Valores por defecto en fecha y hora
+function actualizaFecha(){
+    var fecha =  new Date()
+    console.log(fecha.toISOString());
+    var hoy = fecha.toISOString().slice(0,10)
+
+    console.log(hoy);
+    var ahora = fecha.toLocaleString().split(',')[1].slice(1,6)
+    console.log(ahora);
+
+    let hora = fecha.getHours();
+    let minutos =  fecha.getMinutes();
+    let segundos = fecha.getSeconds();
+    document.getElementById("fecha").value = hoy
+    document.getElementById("hora").value = ahora
+}
+
 
 
 
@@ -40,6 +57,7 @@ document.querySelector('#addTarea').addEventListener('click', (elemento)=>{
 //pointa la lista a partir de localstorage
 
 const pintarLista = ()=>{
+   
     var BDT = localStorage.getItem('tareas')
     if(!BDT){
         BDO = []
@@ -58,11 +76,12 @@ const pintarLista = ()=>{
             <div class="d-flex w-100 justify-content-between">
                 <h5 class="mb-1">${element.titulo}</h5>
                 <small>
-                    <i data-id="${element.id}" class="editar fa-solid fa-pen-to-square ms-2"></i>
+                    <i data-id="${element.id}" class="editar fa-solid fa-pen-to-square ms-2" data-bs-toggle="modal" data-bs-target="#modalEditar"" data-titulo="${element.titulo}" data-fecha="${girarFecha(element.fecha)}" data-hora="${element.hora}"></i>
                     <i data-id="${element.id}" class="borrar fa-solid fa-trash-can ms-2"></i>
+                   
                 </small>
             </div>
-            <p class="mb-1">${element.fecha}</p>
+            <p class="mb-1">${girarFecha(element.fecha)}</p>
             <small>${element.hora}</small>
         </a>
     `
@@ -72,6 +91,7 @@ const pintarLista = ()=>{
     const textArea = document.querySelector('#titulo');
     textArea.value = '';
     textArea.focus();
+    actualizaFecha()
 }
 
 //Borrar todas las tareas
@@ -81,37 +101,69 @@ document.querySelector('#borrarTodo').addEventListener('click', ()=>{
     pintarLista()
 });
 
-//Borrar una tarea
+//Borrar y editar una tarea
 document.querySelector('#lista').addEventListener('click', (UI)=>{
     
     UI.preventDefault();
     console.log(UI.target);
+    //BORRAR
     if(UI.target.classList.contains('borrar')){
-        console.log(UI.target.classList);
         var id = UI.target.dataset.id
-        console.log(id);
-        BDO = JSON.parse(localStorage.getItem('tareas'));
+        BDO = JSON.parse(localStorage.getItem('tareas'));    
        
-        //Tambien puedo obtener array nuevo sin el elemento borrado
-        nuevoBDO = 
-        //borro el dato del array
+        //Obtengo un array con todos los datos menos el que no quiero
         nuevoBDO = BDO.filter(BDO => BDO.id != id)
-        console.log(nuevoBDO);
-
         //actualizo localstorage
         localStorage.setItem('tareas', JSON.stringify(nuevoBDO))
         pintarLista()
+    //EDITAR
+    }else  if(UI.target.classList.contains('editar')){
+        let titulo = UI.target.dataset.titulo
+        let fecha = UI.target.dataset.fecha
+        let hora = UI.target.dataset.hora
+        let id = UI.target.dataset.id
 
-     }
-        
-   
+        document.querySelector('#modal-titulo').value = titulo
+        document.querySelector('#modal-fecha').value = girarFecha(fecha)
+        document.querySelector('#modal-hora').value = hora
+        document.querySelector('#btn-modificar').dataset.id  = id
+    }
 });
 
+//girarFecha
+function girarFecha(fecha){
+    var fechaA = fecha.split('-')
+    fechaGirada = `${fechaA[2]}-${fechaA[1]}-${fechaA[0]}`
+    return fechaGirada
+}
+
 // Ventana modal
-var myModal = document.getElementById('myModal')
-var myInput = document.getElementById('myInput')
+myModal = document.getElementById('modalEditar')
+//var myInput = document.getElementById('#myInput')
 
 myModal.addEventListener('shown.bs.modal', function () {
-  myInput.focus()
+  //myInput.focus()
+  console.log('modal!!!');
 })
+myModal.addEventListener('click', (e)=>{
+    if(e.target.classList.contains('modificar')){
+        console.log('click en modal');
+        BDO = JSON.parse(localStorage.getItem('tareas'))
+        let index = BDO.indexOf(BDO.find(BDO=>BDO.id === e.target.dataset.id))
+    
+        BDO[index].titulo =  document.querySelector('#modal-titulo').value
+        
+        BDO[index].fecha= girarFecha(document.querySelector        
+        ('#modal-fecha').value)
+
+        BDO[index].hora = document.querySelector('#modal-hora').value = '00:00'
+        
+        localStorage.setItem('tareas' , JSON.stringify(BDO))
+        console.log(myModal)
+        pintarLista()
+    }
+})
+
+
+
 pintarLista();
